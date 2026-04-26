72265c25e808ebe603898d1c2389639adeb6265e
akshat917 <bajpaiakshat5089@gmail.com>
2026-04-26 12:37:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 app = Flask(__name__)
 
 SUPABASE_URL = os.getenv("https://mfmzzcxponmcwtpytwev.supabase.co")
-SUPABASE_KEY = os.getenv("sb_publishable_YHz1BALbmb_81__-sifNwA_YR6q2TL7")
+SUPABASE_KEY = os.getenv("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1mbXp6Y3hwb25tY3d0cHl0d2V2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcxNzYwMTIsImV4cCI6MjA5Mjc1MjAxMn0.FW27EhIkKijRQVROZaosXvqU0dEjovDaI44i4ZQy8PI")
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
